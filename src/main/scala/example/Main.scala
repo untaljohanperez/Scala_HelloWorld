7e0d0eb9a0707fa83ff96709152ee4cd0eb6bfb0
Kedwin Johan Pérez Zea - Ceiba Software <kedwin.perez@ceiba.com.co>
2016-09-02 13:12:47 -0500

    recursive sum and greater element in a list

---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,10 @@
+package example
+
+/**
+  * Created by kedwin.perez on 02/09/2016.
+  */
+object Main extends App{
+  var xs = List(1,2,7,6)
+  println(Lists.max(xs) + " is the greater in " + xs)
+  println(Lists.sum(xs) + " is the sum of " + xs)
+}
---
+++ src/main/scala/example/Repl.sc
@@ -0,0 +1,3 @@
+var a: List[Int] = List(1,2,3,4)
+
+a.tail(0)
